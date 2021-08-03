24d0ff2839282c28b3c2a2ab07364b93ce6a92aa
PettersonSantos <petterson.santos@outlook.com.br>
2021-08-03 16:01:23 -0300

    first commit

---
+++ domain/video_test.go
@@ -0,0 +1,14 @@
+package domain_test
+
+import (
+	"project-enconder/domain"
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
